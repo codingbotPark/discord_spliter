275b1dfcadcfa84791b7ae88c141863c73d61525
박병관 <pbk575@gmail.com>
2024-09-17 18:43:29 +0900

    [UPDATE] using routes in app

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express, {Express} from 'express'
 import { Server } from 'http';
 import 'dotenv/config';
 import Logger from './util/Logger';
+import InteractionRouter from './routers/InteractionRouter';
+import Router from './routers/Router';
 
 // App class role = turn on & off express server
 class App {
@@ -13,15 +15,19 @@ class App {
         this.app = express()
         this.logger = new Logger()
         this.initializeMiddleware() // base middleware (ex logger)
-        this.initializeRoute() // base route('interactions' for use discord endpoint)
+        /** @TODO decide class connecting struct */
+        this.initializeRoute([new InteractionRouter()]) // base route('interactions' for use discord endpoint)
     }
 
     private initializeMiddleware(){
         this.app.use(this.logger.logMiddleware) // use logging for every request
     }
 
-    private initializeRoute(){
-        // this.app.use(this.router)
+    private initializeRoute(routers:Router[]){
+        routers.forEach(router => {
+            router.initializeRoutes(); // regist detail routes
+            this.app.use(router.basePath, router.getRouter());
+        });
     }
 
     private getPort(presetPort:number):number{
--- server/src/routers/InteractionRouter.ts
+++ server/src/routers/InteractionRouter.ts
@@ -1,13 +1,16 @@
-    import 'dotenv/config';
-    import { verifyKeyMiddleware } from "discord-interactions";
-    import Router from "./Router";
+import 'dotenv/config';
+import { verifyKeyMiddleware } from "discord-interactions";
+import Router from "./Router";
 
-    class InteractionRouter extends Router{
-        constructor(){
-            super('/interactions')
-        }
-        
-        initializeRoutes(): void {
-            this.registRoute('post', '', verifyKeyMiddleware(process.env.PUBLIC_KEY))
-        }
+class InteractionRouter extends Router {
+    constructor() {
+        super('/interactions')
     }
+
+    initializeRoutes(): void {
+        this.registRoute('post', '', verifyKeyMiddleware(process.env.PUBLIC_KEY), 로직1)
+        this.registRoute('post', '', verifyKeyMiddleware(process.env.PUBLIC_KEY), 로직2)
+    }
+}
+
+export default InteractionRouter
