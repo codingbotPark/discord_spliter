de284f70d959a6a050eca7ee1c720567fcdad73b
박병관 <pbk575@gmail.com>
2024-09-19 17:52:17 +0900

    [ADD] verifyEnv function with 'zod'

--- server/src/util/verifyEnv.ts
+++ server/src/util/verifyEnv.ts
@@ -1,30 +1,37 @@
-import {z} from 'zod'
+import {z, ZodObject} from 'zod'
 import 'dotenv/config'
 
-
 /** @TODO narrowing valid range(ex length)*/
-export const envSchema = z.object({
+const zodObject = {
     PORT: z.number().optional().default(3000),
     APP_ID: z.string(),
     DISCORD_TOKEN: z.string(),
     PUBLIC_KEY: z.string()
-});
+}
+type ZodSchemaType = ZodObject<typeof zodObject>;
 
-const envServer = envSchema.safeParse({
-    PORT:process.env.PORT,
-    APP_ID:process.env.APP_ID,
-    DISCORD_TOKEN:process.env.DISCORD_TOKEN,
-    PUBLIC_KEY:process.env.PUBLIC_KEY
-})
 
-// if (envServer.error){
-//     // logging env setting error
-//     return
-// }
+export const envSchema = z.object(zodObject);
 
-// // data = schema for env
-// const envServerSchema = envServer.data
-// envServerSchema
+export function verifyEnv(envSchema:ZodSchemaType){
+
+    const envServer = envSchema.safeParse({
+        PORT:process.env.PORT,
+        APP_ID:process.env.APP_ID,
+        DISCORD_TOKEN:process.env.DISCORD_TOKEN,
+        PUBLIC_KEY:process.env.PUBLIC_KEY
+    })
+    if (envServer.error){
+        // logging env setting error
+        return
+    }
+    
+    // data = schema for env
+    const envServerSchema = envServer.data
+    return envServerSchema
+}
+
+
 
 
 
